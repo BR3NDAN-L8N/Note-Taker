5b8e335ad4e24064ff25637e408c4406c3dff67e
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-08-31 10:34:43 -0400

    added api routes for get db.json and post new note to db.json

---
+++ routes/apiRoutes.js
@@ -0,0 +1,65 @@
+// 
+// LOAD DATA
+// 
+// linking routes to "data" sources
+const db = require('../db/db.json');
+const uuid = require('uuid');
+
+// 
+// ROUTING
+// 
+module.exports = function (app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+    //  read db.json and returns contents as json
+    app.get("/api/notes", function (req, res) {
+        res.json(db);
+    });
+
+    //   app.get("/api/waitlist", function(req, res) {
+    //     res.json(waitListData);
+    //   });
+
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
+    //  saves user req to db.json
+    app.post("/api/notes", function (req, res) {
+        //  Create object for new note giving random ID
+        const newNote = {
+            id: uuid.v4(),
+            title: req.body.title,
+            text: req.body.text
+        }
+        //  IF user didn't enter a title or text then let them know
+        if (!newNote.title || !newNote.text) {
+            return res.status(400).json({
+                msg: 'You forgot to enter a title or message'
+            });
+        }
+        db.push(newNote);
+        console.log(newNote);
+        res.json(db);
+    });
+
+    // app.delete("/api/notes")
+
+    // ---------------------------------------------------------------------------
+    // I added this below code so you could clear out the table while working with the functionality.
+    // Don"t worry about it!
+
+    //   app.post("/api/clear", function(req, res) {
+    //     // Empty out the arrays of data
+    //     tableData.length = 0;
+    //     waitListData.length = 0;
+
+    //     res.json({ ok: true });
+    //   });
+};
