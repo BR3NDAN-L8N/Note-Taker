2a967f311e32a4d518470099d2cb18dccb00db98
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-09-01 15:34:45 -0400

    added static functionality to index.html

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 //  DEPENDENCIES 
 //
 var express = require("express");
+const path = require('path');
 // ==============
 
 // 
@@ -20,12 +21,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // ==============
 
+// SET STATIC FOLDER
+// .use = used for including middleware
+// __dirname = current directory
+//  public is the name of the folder to make static
+app.use(express.static(path.join(__dirname, './public')));
+// ==============
+
 // 
 // ROUTER
 //
 //  routes for our app to direct traffic
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+// ==============
 
 // 
 // LISTENER
