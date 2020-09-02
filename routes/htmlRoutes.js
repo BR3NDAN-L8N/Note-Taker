f48d07c9889fe10ef55fae3b8a339946c7d1456b
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-09-02 10:27:17 -0400

    getting ready to push to heroku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -16,6 +16,12 @@ const { Router } = require("express");
 //
 module.exports = function (app) {
     // HTML GET Requests directing users to a specific page
+
+    //  directs to index.html
+    app.get("/", function(req, res) {
+        res.json(path.join(__dirname, "public/index.html"));
+      });
+
     // directs to notes.html
     app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
