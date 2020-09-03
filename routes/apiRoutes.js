c5bcb7a1cf0341693c5e2173460fe26531b93841
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-09-03 16:41:20 -0400

    removed redundant comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,27 +11,11 @@ const uuid = require('uuid');
 // ROUTING
 // 
 module.exports = function (app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
     //  read db.json and returns contents as json
     app.get("/api/notes", function (req, res) {
         res.json(db);
     });
 
-    //   app.get("/api/waitlist", function(req, res) {
-    //     res.json(waitListData);
-    //   });
-
-    // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
-    // ---------------------------------------------------------------------------
     //  saves user req to db.json
     app.post("/api/notes", function (req, res) {
         console.log("post")
@@ -41,7 +25,6 @@ module.exports = function (app) {
             title: req.body.title,
             text: req.body.text
         }
-        //  IF user didn't enter a title or text then let them know
 
         db.push(newNote);
 
@@ -50,16 +33,13 @@ module.exports = function (app) {
             if (err) {
                 return console.log(err);
             }
-
             console.log("Success!");
-
         });
         res.json(db);
     });
 
     //   DELETE a note of a specific ID
     app.delete("/api/notes/:id", function (req, res) {
-        console.log('delete sent to backend?');
         const id = req.params.id;
         db.forEach(function (note, index) {
             note.id === id ? db.splice(index, 1) : '';
@@ -68,27 +48,8 @@ module.exports = function (app) {
                     return console.log(err);
                 }
                 console.log("Success!");
-
             });
         });
-
         res.json(db)
-        //you will have to get the array 
-        //find the item (index)
-        //splice
-        //check your api route if it works or console.log db
-        //fs write file
     });
-
-    // ---------------------------------------------------------------------------
-    // I added this below code so you could clear out the table while working with the functionality.
-    // Don"t worry about it!
-
-    //   app.post("/api/clear", function(req, res) {
-    //     // Empty out the arrays of data
-    //     tableData.length = 0;
-    //     waitListData.length = 0;
-
-    //     res.json({ ok: true });
-    //   });
 };
