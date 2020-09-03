8b361cba11aa29a6460482281ab3ec6f1a7b7b35
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-09-03 17:41:17 -0400

    removed redundant comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,13 @@
-// ===============================================================================
 // 
 // DEPENDENCIES
 //
 var path = require("path");
-const { Router } = require("express");
-// var Router =require("express").Router();
 
-// Router.get("/notes", function (req, res){
-//     res.sendFile(path.join(__dirname, "../public/notes.html"));
-// })
-
-// module.exports = router;
 //
 // ROUTING
 //
 module.exports = function (app) {
+
     // HTML GET Requests directing users to a specific page
 
     //  directs to index.html
--- server.js
+++ server.js
@@ -4,31 +4,26 @@
 var express = require("express");
 const path = require('path');
 
-
-// ==============
-
 // 
 // GLOBAL VARIABLES
 // 
 var app = express();
 // Set initial port
 var PORT = process.env.PORT || 8080;
-// ==============
-//app.use("/",  require("./routes/htmlRoutes"));
+
 // 
 //  APP CODE
 // 
 // Sets up the app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// ==============
+
 
 // SET STATIC FOLDER
 // .use = used for including middleware
 // __dirname = current directory
 //  public is the name of the folder to make static
 app.use(express.static(path.join(__dirname, 'public')));
-// ==============
 
 // 
 // ROUTER
@@ -36,7 +31,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 //  routes for our app to direct traffic
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-// ==============
 
 // 
 // LISTENER
@@ -45,4 +39,3 @@ require("./routes/htmlRoutes")(app);
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
-// ==============
